e6cf78c45e1e851edca39859dcd740a5abf6d047
unknown <acsgyorgy.mail@gmail.com>
2025-07-28 16:28:17 +0200

    app ready to develop

---
+++ src/MyEnglishWords.js
@@ -0,0 +1,11 @@
+import './MyEnglishWords.css';
+
+const MyEnglishWords = () => {
+    return (
+        <div className="App">
+      ezaz
+        </div>
+    );
+}
+
+export default MyEnglishWords;
